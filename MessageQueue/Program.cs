e476c7941b8d5512a23c913267f57f40da37c2a3
Mikita Maroz <nikitamoroz118@gmail.com>
2022-06-20 13:47:45 +0200

    Add basic implementation

--- MessageQueue/Program.cs
+++ MessageQueue/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+var connection = factory.CreateConnection();
+
+Console.WriteLine("Connection opened");
+
+var channel = connection.CreateModel();
+
+Console.WriteLine("Channel created");
+
+channel.ExchangeDeclare("DataCaptureExchange", ExchangeType.Direct);
+channel.QueueDeclare("DataCaptureQueue", false, false, false, null);
+channel.QueueBind("DataCaptureQueue", "DataCaptureExchange", "data_capture", null);
+
+Console.WriteLine("Queue created");
+
+Console.ReadKey();
+
+connection.Close();
